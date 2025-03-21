439bc5618ea906b8e183a5d6b0ad8167cde4057c
jhonnyred <grodriguesgalli@gmail.com>
2025-03-21 18:39:54 -0300

    Exercicio de gerenciador Completo

--- exercicio/Main.java
+++ exercicio/Main.java
@@ -1,5 +1,21 @@
 public class Main{
     public static void main(String[] args){
-        
+        GerenciadorTarefas gerenciador = new GerenciadorTarefas();
+
+        // Adicionando Tarefas
+        gerenciador.addTaskRegular("Enviar email para cliente", "22/03/2025");
+        gerenciador.addTaskPrioritaria("Revisar contrato urgente", "22/03/2025");
+        gerenciador.addTaskComPrioridade("Atualizar sistema de segurança", "22/03/2025", 8);
+        gerenciador.addTaskComPrioridade("Ajustar orçamento do projeto", "22/03/2025", 3);
+        gerenciador.addTaskRegular("Organizar reunião de equipe", "23/03/2025");
+        gerenciador.addTaskPrioritaria("Comprar suprimentos para escritório", "23/03/2025");
+
+        // Processando Tarefas
+        gerenciador.proximaTarefa();
+        gerenciador.proximaTarefa();
+        gerenciador.proximaTarefa();
+
+        // Tarefas Pendentes
+        gerenciador.tarefasPendentes();
     }
 }
--- exercicio/Tarefa.java
+++ exercicio/Tarefa.java
@@ -5,10 +5,41 @@ public class Tarefa {
     // ATRIBUTOS
     String descricao;
     LocalDate data;
+    DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-    // CONTRUTOR
+    // CONTRUTORES
+    public Tarefa(){}
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
     public Tarefa(String descricao, String data){
         this.descricao = descricao;
+        this.data = LocalDate.parse(data, formatador);
+    }
+
+    // MÉTODOS
+    public String paraString(){
+        String strdata = data.format(formatador);
+        return descricao +" "+ strdata;
+    }
+
+    // GETTERS
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public LocalDate getData(){
+        return this.data;
+    }
+
+    // SETTERS
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public void setData(String data){
         DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         this.data = LocalDate.parse(data, formatador);
     }
